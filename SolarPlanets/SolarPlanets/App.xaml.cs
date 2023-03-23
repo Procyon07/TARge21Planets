a41d9c3cf5b6739ad576ea3dd1d237417ba0157f
Opilane TTHK <opilane@tthk.ee>
2023-03-23 15:01:55 +0200

    StartPage

--- SolarPlanets/SolarPlanets/App.xaml.cs
+++ SolarPlanets/SolarPlanets/App.xaml.cs
@@ -1,11 +1,39 @@
-﻿namespace SolarPlanets;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.Graphics;
+#endif
+
+
+namespace SolarPlanets;
 
 public partial class App : Application
 {
+
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
+
 	public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+#if WINDOWS
+	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (Handler, view) => 
+		{
+			var mauiWindow = Handler.VirtualView;
+			var nativeWindow = Handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new Windows.Graphics.SizeInt32(WindowWidth, WindowHeight));
+		});	
+
+	
+
+
+#endif
+
+		MainPage = new NavigationPage(new StartPage());
 	}
 }
